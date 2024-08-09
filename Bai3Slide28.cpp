3f71a0b0417284abc633458eb0b2b3c3319ffacd
Tkobietcode <noname220303@gmail.com>
2024-08-09 10:08:35 +0700

    Lam tiep

---
+++ Bai3Slide28.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy tính UCLN của a và b
+int UCLN(int a, int b) {
+    if (b == 0)
+        return a;
+    return UCLN(b, a % b);
+}
+
+int main() {
+    int a = 59, b = 96;
+    printf("UCLN(%d, %d) = %d\n", a, b, UCLN(a, b));
+    return 0;
+}
